c026527f63f8b2fc57b9d434c6c37f1aa9afdac7
Andre Perunicic <perunicic@gmail.com>
2017-04-09 00:55:51 -0400

    Add products crawler and corresponding loader

--- steam/steam/items.py
+++ steam/steam/items.py
@@ -1,31 +1,8 @@
-# -*- coding: utf-8 -*-
 from datetime import datetime, date
 
 import scrapy
-from scrapy.loader.processors import TakeFirst, Compose, Join, MapCompose
+from scrapy.loader.processors import MapCompose, Compose, TakeFirst
 
-
-def simplify_recommended(x):
-    return False if x == 'Not Recommended' else True
-
-
-def strip_text(x):
-    return x.strip(u'\n\t\r')
-
-
-def str_to_float(x):
-    x = x.replace(',', '')
-    try:
-        return float(x)
-    except:
-        x
-
-
-def str_to_int(x):
-    try:
-        return int(str_to_float(x))
-    except:
-        return x
 
 def standardize_date(x):
     """
@@ -37,7 +14,7 @@ def standardize_date(x):
     except ValueError:
         pass
 
-    # Induce year
+    # Induce year.
     try:
         d = datetime.strptime(x, "%B %d")
         d = d.replace(year=date.today().year)
@@ -48,49 +25,24 @@ def standardize_date(x):
     return x
 
 
+def strip_text(x):
+    return x.strip(u'\n\t\r')
+
+
 class ProductItem(scrapy.Item):
+    url = scrapy.Field(output_processor=TakeFirst())
+    id = scrapy.Field(output_processor=TakeFirst())
+    reviews_url = scrapy.Field(output_processor=TakeFirst())
+    title = scrapy.Field(output_processor=TakeFirst())
+    genre = scrapy.Field()
+    developer = scrapy.Field(output_processor=TakeFirst())
+    publisher = scrapy.Field(output_processor=TakeFirst())
+    release_date = scrapy.Field(
+        output_processor=Compose(TakeFirst(), standardize_date)
+    )
     specs = scrapy.Field(
         output_processor=MapCompose(strip_text)
     )
     tags = scrapy.Field(
         output_processor=MapCompose(strip_text)
     )
-
-
-class ReviewItem(scrapy.Item):
-    recommended = scrapy.Field(
-        input_processor=simplify_recommended,
-        output_processor=TakeFirst(),
-    )
-    date = scrapy.Field(
-        output_processor=Compose(TakeFirst(), standardize_date)
-    )
-    text = scrapy.Field(
-        input_processor=MapCompose(strip_text),
-        output_processor=Compose(Join('\n'), strip_text)
-    )
-    hours = scrapy.Field(
-        output_processor=Compose(TakeFirst(), str_to_float)
-    )
-    found_helpful = scrapy.Field(
-        output_processor=Compose(TakeFirst(), str_to_int)
-    )
-    found_unhelpful = scrapy.Field(
-        output_processor=Compose(TakeFirst(), str_to_int)
-    )
-    found_funny = scrapy.Field(
-        output_processor=Compose(TakeFirst(), str_to_int)
-    )
-    compensation = scrapy.Field(
-        output_processor=TakeFirst()
-    )
-    username = scrapy.Field(
-        output_processor=TakeFirst()
-    )
-    user_id = scrapy.Field(
-        output_processor=TakeFirst()
-    )
-    products = scrapy.Field(
-        output_processor=Compose(TakeFirst(), str_to_int)
-    )
-
---
+++ steam/steam/spiders/product_spider.py
@@ -0,0 +1,87 @@
+import re
+from w3lib.url import url_query_cleaner
+
+from scrapy.http import FormRequest, Request
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
+
+from ..items import ProductItem
+
+
+def load_product(response):
+    """Load a ProductItem from the product page response."""
+    loader = ItemLoader(item=ProductItem(), response=response)
+
+    url = url_query_cleaner(response.url, ['snr'], remove=True)
+    loader.add_value('url', url)
+
+    found_id = re.findall('/app/(.*?)/', response.url)
+    if found_id:
+        id = found_id[0]
+        reviews_url = f"http://steamcommunity.com/app/{id}/reviews/?browsefilter=mostrecent&p=1"
+        loader.add_value('reviews_url', reviews_url)
+        loader.add_value('id', id)
+
+    # Publication details.
+    details = response.css('.details_block').extract_first().split('<br>')
+
+    for line in details:
+        line = re.sub('<[^<]+?>', '', line)  # Remove tags.
+        line = re.sub('[\r\t\n]', '', line).strip()
+        for prop, name in [
+            ('Title:', 'title'),
+            ('Genre:', 'genre'),
+            ('Developer:', 'developer'),
+            ('Publisher:', 'publisher'),
+            ('Release Date:', 'release_date')
+        ]:
+            if prop in line:
+                item = line.replace(prop, '').strip()
+                loader.add_value(name, item)
+
+    loader.add_css('specs', '.game_area_details_specs a ::text')
+    loader.add_css('tags', 'a.app_tag::text')
+
+    # price = response.css('.game_purchase_price ::text').extract()
+    # if price:
+    #     loader.add_value('price', price)
+    #     response.css(".discount_original_price ::text").extract()
+
+    return loader.load_item()
+
+
+class ProductSpider(CrawlSpider):
+    name = 'products'
+    start_urls = ["http://store.steampowered.com/search/"]
+    allowed_domains=["steampowered.com"]
+
+    rules = [
+        Rule(LinkExtractor(allow='/app/(.+)/', restrict_css='#search_result_container'),
+             callback='parse_product')
+    ]
+
+    def parse_product(self, response):
+        # Circumvent age check.
+        if '/agecheck/app' in response.url:
+            form = response.css('form')
+
+            action = form.xpath('@action').extract_first()
+            name = form.xpath('input/@name').extract_first()
+            value = form.xpath('input/@value').extract_first()
+
+            formdata = {
+                name: value,
+                'ageDay': '1',
+                'ageMonth': '1',
+                'ageYear': '1955'
+            }
+
+            return FormRequest(
+                url=action,
+                method='POST',
+                formdata=formdata,
+                callback=self.parse_product
+            )
+
+        yield load_product(response)
