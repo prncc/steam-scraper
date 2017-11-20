ab699201f8cfb2733f478a421d7c1de9bd3d517f
Andre Perunicic <perunicic@gmail.com>
2017-11-20 10:38:56 -0500

    Remove unnecessary leftover comments

--- steam/settings.py
+++ steam/settings.py
@@ -5,10 +5,8 @@ BOT_NAME = 'steam'
 SPIDER_MODULES = ['steam.spiders']
 NEWSPIDER_MODULE = 'steam.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
 
-# Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 DOWNLOADER_MIDDLEWARES = {
